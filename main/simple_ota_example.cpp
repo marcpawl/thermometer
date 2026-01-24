009949e56afe03276ba7ee359f83dcd439990d21
Marc Pawlowsky <marcpawl@gmail.com>
2026-01-24 11:39:04 -0500

    Change project to c++20

--- main/simple_ota_example.cpp
+++ main/simple_ota_example.cpp
@@ -7,6 +7,7 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 
+extern "C" {
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_app_desc.h"
@@ -29,6 +30,7 @@
 #if CONFIG_EXAMPLE_CONNECT_WIFI
 #include "esp_wifi.h"
 #endif
+}
 
 #define HASH_LEN 32
 
@@ -77,6 +79,63 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
     return ESP_OK;
 }
 
+static esp_http_client_config_t http_client_config()
+{
+    esp_http_client_config_t config;
+    memset(&config, 0, sizeof(config));
+    config.url = CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL;
+    config.host = nullptr;
+    config.port= 0;
+    config.username  = nullptr;
+    config.password = nullptr;
+    config.auth_type = HTTP_AUTH_TYPE_NONE;
+    config.path = nullptr;
+    config.query = nullptr;
+    config.cert_pem  = nullptr;
+    config.cert_len = 0;
+    config.client_cert_len = 0;
+    config.client_key_pem = nullptr;
+    config.client_key_len = 0;
+    config.client_key_password = nullptr;
+    config.client_key_password_len = 0;
+    config.tls_version = ESP_HTTP_CLIENT_TLS_VER_TLS_1_3;
+    config.user_agent = "marcpawl thermometer";
+    config.method = HTTP_METHOD_GET;
+    config.timeout_ms = 10000;
+    config.disable_auto_redirect = false;
+    config.max_redirection_count = 0;
+    config.max_authorization_retries = 0;
+    config.event_handler = _http_event_handler;
+
+
+#ifdef CONFIG_EXAMPLE_USE_CERT_BUNDLE
+        config.crt_bundle_attach = esp_crt_bundle_attach;
+#else
+        config.cert_pem = (char *) server_cert_pem_start;
+#endif /* CONFIG_EXAMPLE_USE_CERT_BUNDLE */
+        config.keep_alive_enable = true;
+#ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF
+        config.if_name = &ifr;
+#endif
+#if CONFIG_EXAMPLE_TLS_DYN_BUF_RX_STATIC
+        /* This part applies static buffer strategy for rx dynamic buffer.
+         * This is to avoid frequent allocation and deallocation of dynamic buffer.
+         */
+        config.tls_dyn_buf_strategy = HTTP_TLS_DYN_BUF_RX_STATIC;
+#endif /* CONFIG_EXAMPLE_TLS_DYN_BUF_RX_STATIC */
+
+    return config;
+}
+
+
+static esp_https_ota_config_t https_ota_config(esp_http_client_config_t* config)
+{
+    esp_https_ota_config_t ota_config;
+    memset(&ota_config, 0, sizeof(ota_config));
+    ota_config.http_config = config;
+    return ota_config;
+};
+
 static void do_ota(void *pvParameter) {
 #ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF
     esp_netif_t *netif = get_example_netif_from_desc(bind_interface_name);
@@ -88,25 +147,7 @@ static void do_ota(void *pvParameter) {
     esp_netif_get_netif_impl_name(netif, ifr.ifr_name);
     ESP_LOGI(TAG, "Bind interface name is %s", ifr.ifr_name);
 #endif
-    esp_http_client_config_t config = {
-        .url = CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL,
-#ifdef CONFIG_EXAMPLE_USE_CERT_BUNDLE
-        .crt_bundle_attach = esp_crt_bundle_attach,
-#else
-        .cert_pem = (char *) server_cert_pem_start,
-#endif /* CONFIG_EXAMPLE_USE_CERT_BUNDLE */
-        .event_handler = _http_event_handler,
-        .keep_alive_enable = true,
-#ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF
-        .if_name = &ifr,
-#endif
-#if CONFIG_EXAMPLE_TLS_DYN_BUF_RX_STATIC
-        /* This part applies static buffer strategy for rx dynamic buffer.
-         * This is to avoid frequent allocation and deallocation of dynamic buffer.
-         */
-        .tls_dyn_buf_strategy = HTTP_TLS_DYN_BUF_RX_STATIC,
-#endif /* CONFIG_EXAMPLE_TLS_DYN_BUF_RX_STATIC */
-    };
+    esp_http_client_config_t config = http_client_config();
 
 #ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL_FROM_STDIN
     char url_buf[OTA_URL_SIZE];
@@ -126,9 +167,7 @@ static void do_ota(void *pvParameter) {
     config.skip_cert_common_name_check = true;
 #endif
 
-    esp_https_ota_config_t ota_config = {
-        .http_config = &config,
-    };
+    esp_https_ota_config_t ota_config = https_ota_config(&config);
     esp_https_ota_handle_t ota_handle = NULL;
 
     // 1. Connect and start the OTA session
@@ -210,7 +249,7 @@ static void get_sha256_of_partitions(void) {
     print_sha256(sha_256, "SHA-256 for current firmware: ");
 }
 
-void check_version() {
+static void check_version() {
     const esp_app_desc_t *app_desc = esp_app_get_description();
 
     ESP_LOGI(TAG, "Running Version: %s\n", app_desc->version);
@@ -219,7 +258,7 @@ void check_version() {
     ESP_LOGI(TAG, "Compile Time: %s %s\n", app_desc->date, app_desc->time);
 }
 
-void app_main(void) {
+extern "C" void app_main(void) {
     ESP_LOGI(TAG, "OTA example app_main start");
     check_version();
 
