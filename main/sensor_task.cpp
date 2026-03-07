6777839a5bb777ffef0c51031a7e96a9a7727f55
Marc Pawlowsky <marcpawl@gmail.com>
2026-03-07 17:52:47 -0500

    Use Sensor data structure

--- main/sensor_task.cpp
+++ main/sensor_task.cpp
@@ -5,32 +5,35 @@
  * SPDX-License-Identifier: Unlicense OR CC0-1.0
  */
 
+extern "C" {
 #include <stdio.h>
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "ds18b20.h"
 #include "onewire_bus.h"
-/*
- * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Unlicense OR CC0-1.0
- */
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "esp_check.h"
-#include "onewire_bus.h"
-#include "ds18b20.h"
+}
+#include "marcpawl_inplace_vector.hpp"
 
-static const char *TAG = "example";
+static const char *TAG = "sensor_task";
 
 #define EXAMPLE_ONEWIRE_BUS_GPIO    4
 #define EXAMPLE_ONEWIRE_MAX_DS18B20 2
 
+/** Sensor readings of this value or larger are invalid. */
+constexpr float max_temperature = 85.0;
+
+struct Sensor
+{
+    ds18b20_device_handle_t handle;
+    onewire_device_address_t address;
+};
+
+using Sensors = marcpawl::inplace_vector<Sensor, EXAMPLE_ONEWIRE_MAX_DS18B20 >;
+
 void sensor_task(void *pvParameters)
 {
-    // install new 1-wire bus
+    // install a new 1-wire bus
     onewire_bus_handle_t bus;
     onewire_bus_config_t bus_config = {
         .bus_gpio_num = EXAMPLE_ONEWIRE_BUS_GPIO,
@@ -44,8 +47,7 @@ void sensor_task(void *pvParameters)
     ESP_ERROR_CHECK(onewire_new_bus_rmt(&bus_config, &rmt_config, &bus));
     ESP_LOGI(TAG, "1-Wire bus installed on GPIO%d", EXAMPLE_ONEWIRE_BUS_GPIO);
 
-    int ds18b20_device_num = 0;
-    ds18b20_device_handle_t ds18b20s[EXAMPLE_ONEWIRE_MAX_DS18B20];
+    Sensors ds18b20s;
     onewire_device_iter_handle_t iter = NULL;
     onewire_device_t next_onewire_device;
     esp_err_t search_result = ESP_OK;
@@ -57,36 +59,42 @@ void sensor_task(void *pvParameters)
         search_result = onewire_device_iter_get_next(iter, &next_onewire_device);
         if (search_result == ESP_OK) { // found a new device, let's check if we can upgrade it to a DS18B20
             ds18b20_config_t ds_cfg = {};
-            if (ds18b20_new_device(&next_onewire_device, &ds_cfg, &ds18b20s[ds18b20_device_num]) == ESP_OK) {
-                ESP_LOGI(TAG, "Found a DS18B20[%d], address: %016llX", ds18b20_device_num, next_onewire_device.address);
-                ds18b20_device_num++;
-                if (ds18b20_device_num >= EXAMPLE_ONEWIRE_MAX_DS18B20) {
-                    ESP_LOGI(TAG, "Max DS18B20 number reached, stop searching...");
-                    break;
-                }
+            ds18b20_device_handle_t handle;
+            if (ds18b20_new_device(&next_onewire_device, &ds_cfg, &handle) == ESP_OK) {
+                ESP_LOGI(TAG, "Found a DS18B20 address: " PRIX64, next_onewire_device.address);
+                ds18b20s.emplace_back(handle, next_onewire_device.address);
             } else {
                 ESP_LOGI(TAG, "Found an unknown device, address: %016llX", next_onewire_device.address);
             }
         }
     } while (search_result != ESP_ERR_NOT_FOUND);
     ESP_ERROR_CHECK(onewire_del_device_iter(iter));
-    ESP_LOGI(TAG, "Searching done, %d DS18B20 device(s) found", ds18b20_device_num);
+    ESP_LOGI(TAG, "Searching done, %d DS18B20 device(s) found", ds18b20s.size());
 
     // set resolution for all DS18B20s
-    for (int i = 0; i < ds18b20_device_num; i++) {
+    for (auto const& sensor : ds18b20s) {
         // set resolution
-        ESP_ERROR_CHECK(ds18b20_set_resolution(ds18b20s[i], DS18B20_RESOLUTION_12B));
+        ESP_ERROR_CHECK(ds18b20_set_resolution(sensor.handle, DS18B20_RESOLUTION_12B));
     }
 
     // get temperature from sensors one by one
-    float temperature;
-    while (1) {
+    while (true) {
+        for (auto const& sensor : ds18b20s)
+        {
+            ESP_ERROR_CHECK(ds18b20_trigger_temperature_conversion(sensor.handle));
+        }
         vTaskDelay(pdMS_TO_TICKS(200));
 
-        for (int i = 0; i < ds18b20_device_num; i ++) {
-            ESP_ERROR_CHECK(ds18b20_trigger_temperature_conversion(ds18b20s[i]));
-            ESP_ERROR_CHECK(ds18b20_get_temperature(ds18b20s[i], &temperature));
-            ESP_LOGI(TAG, "temperature read from DS18B20[%d]: %.2fC", i, temperature);
+        for (auto const& sensor : ds18b20s) {
+            float temperature;
+            ESP_ERROR_CHECK(ds18b20_get_temperature(sensor.handle, &temperature));
+            ESP_LOGI(TAG, "temperature read from " PRIX64 ": %.2fC", sensor.handle, temperature);
+            if (temperature >= max_temperature)
+            {
+                ESP_LOGE(TAG, "Maximum temperature exceeded " PRIX64 ": %.2fC", sensor.handle, temperature);
+            }
         }
+
+        vTaskDelay(pdMS_TO_TICKS(200));
     }
 }
